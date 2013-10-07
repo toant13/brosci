45e3703dac3de9f633c0a21726f88128b304ae05
toantran <toant713@gmail.com>
2013-10-06 19:44:05 -0700

    Fixed db location

--- RegiWorkoutV1/RegiWorkoutV1/settings.py
+++ RegiWorkoutV1/RegiWorkoutV1/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/toantran/Documents/Java_development/workspace/RegiWorkoutV1/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/toantran/git/Regi/RegiWorkoutV1/sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
