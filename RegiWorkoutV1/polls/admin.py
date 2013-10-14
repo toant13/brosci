cdc30ff0153654524fa5ce21fa7cf7dff6bfed4c
toantran <toant713@gmail.com>
2013-10-14 13:14:15 -0700

    Added website templates

--- RegiWorkoutV1/polls/admin.py
+++ RegiWorkoutV1/polls/admin.py
@@ -5,6 +5,25 @@ Created on Oct 6, 2013
 '''
 
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
 
-admin.site.register(Poll)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    
+admin.site.register(Poll, PollAdmin)
+
+
+admin.site.register(Choice)
