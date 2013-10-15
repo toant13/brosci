edf199d7cb68e9df775a3fb246f2211ff11b9a5e
toantran <toant713@gmail.com>
2013-10-14 17:15:35 -0700

    Updated views with correct parameters

--- RegiWorkoutV1/polls/views.py
+++ RegiWorkoutV1/polls/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the poll index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at question %s." % poll_id)
+
+def results(request, poll_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on question %s." % poll_id)
