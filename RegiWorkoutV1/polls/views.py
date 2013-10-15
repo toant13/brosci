92b7ac7c78851220246292c6b4807f7542ba7a9b
toantran <toant713@gmail.com>
2013-10-14 17:49:03 -0700

    Added index and details page. Updated views.py for new pages.

--- RegiWorkoutV1/polls/views.py
+++ RegiWorkoutV1/polls/views.py
@@ -1,11 +1,20 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from polls.models import Poll
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at question %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     response = "You're looking at the results of question %s."
