85ea5c4e86b4381067c250bf18d27f41b4b70343
toantran <toant713@gmail.com>
2013-10-14 21:44:54 -0700

    made urls more generic

--- RegiWorkoutV1/polls/views.py
+++ RegiWorkoutV1/polls/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Poll
-from django.http import Http404
+
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -10,10 +10,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
